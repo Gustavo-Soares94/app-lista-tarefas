c961115048173611ac3d430d2f458eac8a2b745d
Gustavo Soares <gustavosoares.adm@gmail.com>
2021-03-21 17:46:20 -0300

    Primeiro commit, telas de Splash, Login e Principal

---
+++ app/src/main/java/com/example/listatarefas/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.listatarefas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btLogin = (Button) findViewById(R.id.bt_login);
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText txtLogin = (EditText) findViewById(R.id.txt_login);
+                EditText txtSenha = (EditText) findViewById(R.id.txt_senha);
+                String login = txtLogin.getText().toString();
+                String senha = txtSenha.getText().toString();
+                if (login.equals(senha)) {
+                    Intent intent3 = new Intent(getApplicationContext(), TelaPrincipalActivity.class);
+                    startActivity(intent3);
+                } else{
+                        alert("Nome e Senha devem ser iguais");
+                }
+            }
+        });
+    }
+
+    public void irTelaPrincipal(View view){
+
+        Intent intent3 = new Intent(getApplicationContext(), TelaPrincipalActivity.class);
+        startActivity(intent3);
+    }
+
+    private void alert(String s){
+
+        Toast.makeText(this,s,Toast.LENGTH_LONG).show();
+    }
+
+}
---
+++ app/src/main/java/com/example/listatarefas/TelaPrincipalActivity.java
@@ -0,0 +1,29 @@
+package com.example.listatarefas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class TelaPrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_principal);
+
+    }
+
+    public void voltarLogin(View view){
+
+        Intent intent1 = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent1);
+    }
+
+    public void voltarSplash(View view){
+
+        Intent intent2 = new Intent(getApplicationContext(), SplashActivity.class);
+        startActivity(intent2);
+    }
+}
